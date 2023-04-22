a1d73aa4a3b8112f49d85824d8429a0493babfe2
thpeng <thierry.peng@gmail.com>
2023-04-22 17:10:58 +0200

    Initial commit

---
+++ src/main/java/ch/hwz/dpl/mpwc/MicropaymentsWithCrpytoApplication.java
@@ -0,0 +1,13 @@
+package ch.hwz.dpl.mpwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicropaymentsWithCrpytoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicropaymentsWithCrpytoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/hwz/dpl/mpwc/MicropaymentsWithCrpytoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hwz.dpl.mpwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicropaymentsWithCrpytoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
